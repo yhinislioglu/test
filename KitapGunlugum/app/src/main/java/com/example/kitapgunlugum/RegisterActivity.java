a802000e8280c73f0c2a90a4b0b3d298b9ad23ac
yhinislioglu <y.hinislioglu@hotmail.com>
2019-07-11 17:29:05 +0300

    11072019

--- KitapGunlugum/app/src/main/java/com/example/kitapgunlugum/RegisterActivity.java
+++ KitapGunlugum/app/src/main/java/com/example/kitapgunlugum/RegisterActivity.java
@@ -1,7 +1,14 @@
 package com.example.kitapgunlugum;
 
+import android.graphics.drawable.Drawable;
+import android.support.annotation.Nullable;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.SpinnerAdapter;
+import android.support.v7.app.ActionBar;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -9,5 +16,15 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        Toolbar toolbar = findViewById(R.id.toolbar2);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        overridePendingTransition(R.anim.slide_out_left,R.anim.slide_in_right);
+
     }
+
+
+
+
+
 }
